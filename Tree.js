49aa8102be88e50997e0f654f1ba1031003ab7b6
Randy Kim <rhkim1292@gmail.com>
2022-11-05 20:37:41 -0700

    Implement the Node and Tree classes
    The Node class represents a single node in the binary search tree which
    consists of a data value, a pointer to a left child, and a pointer to a
    right child.
    
    The Tree class contains all the methods necessary for a binary search
    tree to function. The only property a Tree class contains is the root
    node of the binary search tree. A buildTree function has also been
    implemented in order to initialize a balanced binary search tree when
    an array is given to a constructor of a Tree class.

---
+++ Tree.js
@@ -0,0 +1,76 @@
+import Node from './Node.js';
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.val}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+class Tree {
+  constructor(treeArr) {
+    const uniqueSorted = this.removeDuplicates(this.mergeSort(treeArr));
+    this.root = this.buildTree(uniqueSorted, 0, uniqueSorted.length - 1);
+  }
+
+  mergeSort(arr) {
+    if (arr.length < 2) return arr;
+    const halfwayPoint = Math.floor(arr.length / 2);
+    const firstHalf = arr.slice(0, halfwayPoint);
+    const secondHalf = arr.slice(halfwayPoint, arr.length);
+    const sortedFirstHalf = this.mergeSort(firstHalf);
+    const sortedSecondHalf = this.mergeSort(secondHalf);
+
+    let i = 0;
+    let j = 0;
+    const sorted = [];
+    while (i < sortedFirstHalf.length && j < sortedSecondHalf.length) {
+      if (sortedFirstHalf[i] < sortedSecondHalf[j]) {
+        sorted.push(sortedFirstHalf[i]);
+        i++;
+      } else {
+        sorted.push(sortedSecondHalf[j]);
+        j++;
+      }
+    }
+
+    if (i < sortedFirstHalf.length) {
+      while (i < sortedFirstHalf.length) {
+        sorted.push(sortedFirstHalf[i]);
+        i++;
+      }
+    } else if (j < sortedSecondHalf.length) {
+      while (j < sortedSecondHalf.length) {
+        sorted.push(sortedSecondHalf[j]);
+        j++;
+      }
+    }
+
+    return sorted;
+  }
+
+  removeDuplicates(arr) {
+    const unique = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (!unique.includes(arr[i])) unique.push(arr[i]);
+    }
+    return unique;
+  }
+
+  buildTree(treeArr, start, end) {
+    if (start > end) {
+        return null;
+    }
+    const mid = parseInt((start + end) / 2);
+    const node = new Node(treeArr[mid]);
+    node.left = this.buildTree(treeArr, start, mid - 1);
+    node.right = this.buildTree(treeArr, mid + 1, end);
+    return node;
+  }
+}
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(tree.root);
